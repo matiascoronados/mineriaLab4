429081db54084cce6eb266f59187a0a029983965
Matias <matias.coronado@usach.cl>
2021-06-14 03:22:35 -0400

    Codigo al 30%

--- lab3.R
+++ lab3.R
@@ -25,6 +25,7 @@ print(res_hc)
 # Red bayeciana con 'lista negra' de relaciones
 bl <- data.frame('M..Work',"Family")
 res_hc_bl <- hc(bn_df, blacklist = bl)
+
 plot(res_hc_bl)
 print(res_hc_bl)
 
@@ -55,7 +56,8 @@ query1 <- cpquery(fittedbn, event = (Proteins == ">3"), evidence = ((Smoking=="n
 ########################################################
 
 ############################
-# Algoritmo Hill-Climbing
+# Metodo de gradiente estodastico
+#   Algoritmo Hill-Climbing
 ############################
 as_df <- data.frame(asia)
 res <- hc(as_df)
@@ -64,7 +66,6 @@ sc <- score(res,as_df)
 print(sc)
 fittedbn <- bn.fit(res,data=as_df)
 print(fittedbn$E)
-
 
 ############################
 # Algoritmo Max-Min Hill Climbing
@@ -75,6 +76,7 @@ sc <- score(res,as_df)
 print(sc)
 fittedbn <- bn.fit(res,data=as_df)
 print(fittedbn$E)
+
 
 ############################
 # Max-Min Parents & Children
@@ -92,11 +94,18 @@ print(fittedbn$E)
 ####################################################################################
 ####################################################################################
 
-
 require(bnlearn)
-install.packages("usethis")
-usethis::use_course("https://goo.gl/x9rdpD")
+#install.packages("usethis")
+#usethis::use_course("https://goo.gl/x9rdpD")
 data(alarm)
+
+bif <- read.bif("/Users/matiascoronado/Desktop/mineriaLab4/ALARM/alarm.bif")
+net <- read.net("/Users/matiascoronado/Desktop/mineriaLab4/ALARM/alarm.net")
+dsc <- read.dsc("/Users/matiascoronado/Desktop/mineriaLab4/ALARM/alarm.dsc")
+rds <- readRDS("/Users/matiascoronado/Desktop/mineriaLab4/ALARM/alarm.rds")
+
+#""""""""""Pre-procesamiento""""""""""
+data = alarm[,c(1:37)]
 
 modelstring = paste0("[HIST|LVF][CVP|LVV][PCWP|LVV][HYP][LVV|HYP:LVF][LVF]",
                      "[STKV|HYP:LVF][ERLO][HRBP|ERLO:HR][HREK|ERCA:HR][ERCA][HRSA|ERCA:HR][ANES]",
@@ -104,17 +113,43 @@ modelstring = paste0("[HIST|LVF][CVP|LVV][PCWP|LVV][HYP][LVV|HYP:LVF][LVF]",
                      "[SAO2|PVS:SHNT][PAP|PMB][PMB][SHNT|INT:PMB][INT][PRSS|INT:KINK:VTUB][DISC]",
                      "[MVS][VMCH|MVS][VTUB|DISC:VMCH][VLNG|INT:KINK:VTUB][VALV|INT:VLNG]",
                      "[ACO2|VALV][CCHL|ACO2:ANES:SAO2:TPR][HR|CCHL][CO|HR:STKV][BP|CO:TPR]")
+
 dag = model2network(modelstring)
+graphviz.plot(dag, layout = "dot")
+
+# Algoritmo Hill-Climbing
+res1 <- hc(data)
+graphviz.plot(res1, layout = "dot")
+sc1 <- score(res1,data)
+print(sc1)
+fittedbn1 <- bn.fit(res1,data=data)
+print(fittedbn1)
+
+#Compara las aristas de res1 y dag
+#Indica que modificaicones se tienen que realizar en res1 para que sea igual a dag
+shd(res1,dag,debug = TRUE)
+
+#unlist(compare(res1,dag)) entrga una matriz con los valores de: tp fp fn
+# En el ejemplo se elige el modelo que entrege el mayor tp
+#     Filo lo anterior, utilizemos solo el BIC
+print(unlist(compare(res1,dag)))
+
+#Comparacion inutil
+#compare(res1,dag, arcs = TRUE)
 
 
+# Algoritmo Max-Min Hill Climbing
+res2 <- mmhc(data)
+graphviz.plot(res2, layout = "dot")
+sc2 <- score(res2,data)
+print(sc2)
+fittedbn <- bn.fit(res2,data=data)
+print(fittedbn)
 
-
-
-
-
-
-
-
-
-
-
+# Max-Min Parents & Children (NO SIRVE)
+#res <- mmpc(data)
+#graphviz.plot(res, layout = "dot")
+#sc <- score(res,data)
+#print(sc)
+#fittedbn <- bn.fit(res,data=data)
+#print(fittedbn)
